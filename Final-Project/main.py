f8834d982786c17507bd46f1502eac48dbad7418
Edward Lloyd Dickenson <EdwardLDickenson@users.noreply.github.com>
2017-06-07 14:18:37 -0500

    Add files via upload

--- Final-Project/main.py
+++ Final-Project/main.py
@@ -11,32 +11,60 @@ jinjaTemplate = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
 autoescape=True)
 
+#   Going to be the values for/from the undo and redo buttons
 operatorStack = [];
 redoStack = [];
+
+class evtLocation(ndb.Model):
+    x = ndb.IntegerProperty();
+    y = ndb.IntegerProperty();
+
+class Operation(ndb.Model):
+    url = ndb.TextProperty();
+    evt = ndb.StringProperty();
+    location = ndb.StructuredProperty(evtLocation, repeated=True);    
 
 class Home(webapp2.RequestHandler):
     def get(self):
         page = jinjaTemplate.get_template('/HTML/index.html');
+        #   Not actually passing anything to the page yet, but this might be the way we load the saved image
         values = {};
         
         self.response.write(page.render(values));
     
     def post(self):
+       
+        data = self.request.body;
+        vals = json.loads(data);
+        
+        if type(vals) is list:
+            print("Repeated");
+            
+        if type(vals) is dict:
+            print("Not repeated");
+        
+        #if vals[0] != "evt":
+        #    print("repeated");
+        
+        #if len(vals) > 5:
+        #    print(len(vals));
+        #    print(vals);
+            
+        #print(vals["url"]);
+        
+        #print(vals)
+        #print(len(vals));
+        
+        #operation = Operation(location=);
+        #   Has to be -1 because the last index contains event type 
+        #for i in range(len(vals) - 1):
+            
+         
+        #print(vals);
+        #operation = Operation(url=vals["url"], evt=vals["evt"]);
+        #operation.put();
+        
         pass;
-        #   Don't be confused, these are keys, not indicies. The
-        #   actual index is + 2 of the key. The first index is the 
-        #   name of the function, which will be used as the value 
-        #   for the function map, and the second one is used for 
-        #   the "argc"
-        
-        #print(self.request.get("evt"));
-        
-        #for i in range(int(self.request.get("argc"))):
-        #    print(self.request.get(str(i)))
-        
-        data = self.request.body
-        vals = json.loads(data)
-        print(vals)
         
 app = webapp2.WSGIApplication([
     ('/', Home)
